f375d3068ac97d12f08222370b920dd67efcdf02
AvaterClasher <avater.clasher@gmail.com>
2024-02-10 19:16:40 +0530

    feat: :sparkles: added a script for faster java class file generation

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,2 @@
 pub mod class;
+mod buffer;
---
+++ tests/resources/Fejvm/hi.java
@@ -0,0 +1,28 @@
+package Fejvm;
+
+public class hi {
+    private final double real;
+    private final double imag;
+
+    public hi(double real) {
+        this.real = real;
+        this.imag = 0;
+    }
+
+    public hi(double real, double imag) {
+        this.real = real;
+        this.imag = imag;
+    }
+
+    public double getReal() {
+        return this.real;
+    }
+
+    public double getImag() {
+        return this.imag;
+    }
+
+    public double abs() {
+        return Math.sqrt(this.real * this.real + this.imag * this.imag);
+    }
+}
