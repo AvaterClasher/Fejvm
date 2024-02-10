a3d9d445a3976d585d98a402c2f063c7090a9abf
AvaterClasher <avater.clasher@gmail.com>
2024-02-10 19:32:07 +0530

    feat: :sparkles: refactor buffer classs

--- src/class.rs
+++ src/class.rs
@@ -11,7 +11,7 @@ pub type ClassResult = Result<ClassFile>;
 
 pub fn read(data: &[u8]) -> ClassResult {
     let mut reader = BufferReader::new(data);
-    let magic = reader.next_u32()?;
+    let magic = reader.read_u32();
     if magic != 0xCAFEBABE {
         return Err(Error::new(ErrorKind::InvalidData, "No magic number"));
     }
